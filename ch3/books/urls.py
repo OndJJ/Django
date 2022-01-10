8c024f0f27dc9d4bb6ac8916dcbd055faca3e185
JeonChangJun <jcjk0302@gmail.com>
2022-01-10 20:00:21 +0900

    220110_2

--- ch3/books/urls.py
+++ ch3/books/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('book/', views.BookList.as_view(), name='book_list'),   # /books/book/
     path('author/', views.AuthorList.as_view(), name='author_list'), # /books/author/
     path('publisher/', views.PublisherList.as_view(), name='publisher_list'), # /book/publisher/
-    path('book/<int:pk>/', views.AuthorDetail.as_view(), name='author_detail'), # /books/author/99/
+    path('book/<int:pk>/', views.BookDetail.as_view(), name='book_detail'), # /books/book/99
+    path('author/<int:pk>/', views.AuthorDetail.as_view(), name='author_detail'), # /books/author/99/
     path('publisher/<int:pk>/', views.PublisherDetail.as_view(), name='publisher_detail'), # /books/publisher/88/
 ]
